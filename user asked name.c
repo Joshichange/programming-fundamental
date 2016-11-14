cbc8e7c25ad10767dfa85fe6e108bbf9404d83f9
change.Joshi <change.joshi247@gmail.com>
2016-11-14 12:58:01 +0545

    Add files via upload

---
+++ user asked name.c
@@ -0,0 +1,21 @@
+/*
+NAME: CHANGE JOSHI
+SUBJECT: PROGRAMMING FUNDAMENTAL
+ROLL NO: 5
+lAB SHEET NO : 1
+PROGRAM: WRITING A C PROGRAM TO PRINT  ASK MY NAME.
+Date :
+*/
+
+#include <stdio.h>
+#include <conio.h>
+int main (){
+char name [11];
+
+printf ("What is your name?\n");
+scanf ("%s", &name );
+printf ("name of user is: %s", name);
+
+getch();
+return 0;
+}
