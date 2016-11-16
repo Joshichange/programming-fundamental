5546c6f3b3e2c7afb72869cc80d9aee07b77e0e2
change.Joshi <change.joshi247@gmail.com>
2016-11-16 12:26:20 +0545

    Add files via upload

---
+++ dividend of.c
@@ -0,0 +1,22 @@
+/*
+name: change G.l Joshi
+subjects: Programming fundamental
+lab sheet no: 2
+Roll no: 5
+Program: WAP to print sum of two numbers, asked by user
+date: 16th nov,2016
+*/
+#include <stdio.h>
+#include <conio.h>
+int main () {
+float a, b, divide ;
+printf ("enter the first number");
+scanf("%f", &a);
+printf ("enter the second number");
+scanf("%f", &b);
+divide = a / b ;
+
+printf ("the devidend of two no. is %f",divide);
+getch () ;
+return 0;
+}
