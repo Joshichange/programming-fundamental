f3da1dec3c4d9c6b850e7fd64261438a8e48dc52
change.Joshi <change.joshi247@gmail.com>
2016-11-16 10:37:12 +0545

    Add files via upload

---
+++ product of no.c
@@ -0,0 +1,22 @@
+/*
+name: change G.l Joshi
+subjects: Programming fundamental
+lab sheet no: 2
+Roll no: 5
+Program: WAP to print sum of two numbers, asked by user
+date: 16th nov,2016
+*/
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int a, b, product ;
+printf ("enter the first no ");
+scanf ("%d", &a);
+printf ("enter the second no ");
+scanf ("%d", &b);
+product = a* b;
+printf("the product of two value is %d", product);
+
+getch ();
+return 0 ;
+}
---
+++ sum of number.c
@@ -0,0 +1,22 @@
+/*
+name: change G.l Joshi
+subjects: Programming fundamental
+lab sheet no: 2
+Roll no: 5
+Program: WAP to print sum of two numbers, asked by user
+date: 16th nov,2016
+*/
+#include <stdio.h>
+#include <conio.h>
+int main (){
+int a, b, sum ;
+printf ("enter the first no ");
+scanf ("%d", &a);
+printf ("enter the second no ");
+scanf ("%d", &b);
+sum = a+b;
+printf("the sum of two value is %d", sum);
+scanf ("%d",&sum);
+getch ();
+return 0 ;
+}
