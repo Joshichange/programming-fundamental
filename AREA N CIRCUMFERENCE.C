f45d232a9c9a1a8a366b4b8460962b0aa4672058
change G.L Joshi <change.joshi247@gmail.com>
2016-11-18 12:30:23 +0545

    Add files via upload

---
+++ AREA N CIRCUMFERENCE.C
@@ -0,0 +1,20 @@
+/*
+name: change joshi
+subject: programmimg fundamental
+roll no: 5
+program: wap to print simple interest
+date: 18 nov 2016
+*/
+#include <stdio.h>
+#include <conio.h>
+int main (){
+float r, area, circumference;
+printf("Enter the radius:\n");
+scanf("%f", &r);
+area=3.14*r*r;
+circumference=2*3.14*r;
+printf("The area and circumference of circle is %f %f",area, circumference);
+getch();
+return 0;
+
+}
