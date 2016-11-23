6aae7cdbe834bae655bb329f8f011906698327da
change G.L Joshi <change.joshi247@gmail.com>
2016-11-23 10:08:01 +0545

    Add files via upload

---
+++ sumofdigit.c
@@ -0,0 +1,23 @@
+/*
+name: change G.l Joshi
+subjects: Programming fundamental
+lab sheet no: 2
+Roll no: 5
+Program: WAP to print sum of two numbers, asked by user
+date: 23th nov,2016
+*/
+#include<stdio.h>
+
+int main (){
+int a, b,c, sum ;
+printf ("enter the first digits\n ");
+scanf ("%d", &a);
+printf ("enter the second digits\n ");
+scanf ("%d", &b);
+printf("Enter the third digits\n");
+scanf("%d", &c);
+sum = a+b+c;
+printf("the sum of digits is %d", sum);
+
+return 0 ;
+}
