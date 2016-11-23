65a79a0b74c974f6c4758f49817f4b0710e4a2b8
change G.L Joshi <change.joshi247@gmail.com>
2016-11-23 12:43:57 +0545

    Add files via upload

---
+++ asciivalue.c
@@ -0,0 +1,18 @@
+/*
+name: change G.l Joshi
+subjects: Programming fundamental
+lab sheet no: 2
+Roll no: 5
+Program: WAP to read ascii from user? 
+date: 23th nov,2016
+*/
+#include<stdio.h>
+int main (){
+	char a;
+	int asciivalue;
+	printf("Enter the character\n");
+	scanf("%c",&a);
+	asciivalue = a;
+	printf("the ascii value is %d",asciivalue);
+	return 0;
+	}
