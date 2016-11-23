1159578ac453afbb3e66b1eba05c2de141a35cb7
change G.L Joshi <change.joshi247@gmail.com>
2016-11-23 12:46:28 +0545

    Add files via upload

---
+++ quadratice.c
@@ -0,0 +1,25 @@
+/*
+name: change G.l Joshi
+subjects: Programming fundamental
+lab sheet no: 2
+Roll no: 5
+Program: WAP to read quadratic equation and its value? 
+date: 23th nov,2016
+*/
+#include<stdio.h>
+#include<math.h>
+int main(){
+float x1, x2, a, b, c, dis;
+printf("Enter the first value\n");
+scanf("%f",&a);
+printf("Enter the second value\n");
+scanf("%f",&b);
+printf("Enter the third value\n");
+scanf("%f",&c);
+dis=b*b-4*a*c;
+x1=-b+sqrt(dis)/2*a;
+x2=-b-sqrt(dis)/2*a;
+printf("the quadratic equation is positive: %f",x1);
+printf("the value of equation is negative: %f",x2);
+return 0;
+}
