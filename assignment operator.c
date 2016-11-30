4916d7e4dc1348c3c761f0467befcb07a9a393b7
change G.L Joshi <change.joshi247@gmail.com>
2016-11-30 02:15:07 -0500

    Add files via upload

---
+++ assignment operator.c
@@ -0,0 +1,30 @@
+/*
+ Name: Change Joshi
+Class:Bcs  
+Roll no.:4
+labsheet:8  
+Program:Wap to use assignment operater
+*/
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	printf("write the first no");
+	scanf("%d",&a);
+	printf("write the second no");
+	scanf("%d",&b);
+	a+=b;
+	printf(" The sum of +=%d\n",a);
+	a-=b;
+	printf("The subtraction -=%d\n",a);
+	a*=b;
+	printf(" The multiplication of *=%d\n",a);
+	a/=b;
+	printf("The division /=%d\n",a);
+	a^=b;
+	printf(" The up of ^=%d\n",a;
+	return 0;
+	
+}
+	
+	
