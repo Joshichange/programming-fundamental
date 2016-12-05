308688b817a2c1c99b08383ebe785d24aec0339a
change G.L Joshi <change.joshi247@gmail.com>
2016-12-05 11:01:45 +0545

    Add files via upload

--- relationaloperator.c
+++ relationaloperator.c
@@ -12,17 +12,25 @@ int main(){
     float a;
     printf("Enter the marks\n");
     scanf("%f",&a);
-    if(a>80 && a<100){
-    printf("Congrats you passed in distintion");
+    if (a>=35)
+    {
+        printf ("you have passed");
+    }
+    else
+    {
+        printf("You have failed");
+    }
+    if(a>80&&a<100){
+    printf("\nCongrats you passed in distintion");
     }
     if(a>60 && a<80){
-        printf("You passed in first division");
+        printf("\nYou passed in first division");
     }
     if(a>45 && a<60){
-        printf("You passed in second division ");
+        printf("\nYou passed in second division ");
     }
     if(a>35 && a<45){
-        printf("You passed in third division");
+        printf("\nYou passed in third division");
     }
 getch();
 return 0;
