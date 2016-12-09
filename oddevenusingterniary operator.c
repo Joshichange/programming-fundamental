f0669bcdae395bea061cad0c8e1e282065d98ca4
change G.L Joshi <change.joshi247@gmail.com>
2016-12-09 10:27:34 +0545

    Add files via upload

---
+++ oddevenusingterniary operator.c
@@ -0,0 +1,22 @@
+/*
+Student Name :Change joshi
+Subject : Programming Fundamental
+Roll No: 12
+Lab Sheet No:11
+Program : WAP to check no.is odd or even using terniary operator?
+Date; December 06 2016
+*/
+
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+int a;
+print("Enter the number:");
+scanf("%d",&a);
+(a%2== 0)?printf("The number is even"):printf("The number is odd");
+
+getch();
+return(0);
+}
+
