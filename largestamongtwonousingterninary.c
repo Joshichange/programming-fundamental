116c8b4edccc34a0471c84647093e10bcd0fc347
change G.L Joshi <change.joshi247@gmail.com>
2016-12-09 10:50:34 +0545

    Add files via upload

---
+++ largestamongtwonousingterninary.c
@@ -0,0 +1,23 @@
+/*
+Student Name :Change joshi
+Subject : Programming Fundamental
+Roll No: 12
+Lab Sheet No:11
+Program : WAP to check no.is odd or even using terniary operator?
+Date; December 06 2016
+*/
+#include <stdio.h>
+#include <conio.h>
+int main(){
+int a,b;
+printf("Enter the first no. is :");
+scanf("%d",&a);
+printf("Enter the second no. is");
+scanf("%d",&b);
+(a>=b)?printf("the largest no. is a %d",a):printf("The largest no. is b %d",b);
+
+
+getch ();
+return 0;
+
+}
