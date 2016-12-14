e44e60ba64721406535ea714020bf78c4bb0bac7
change G.L Joshi <change.joshi247@gmail.com>
2016-12-14 10:49:56 +0545

    Add files via upload

---
+++ divisibleby5and11.c
@@ -0,0 +1,20 @@
+Name:Change G.L Joshi
+Subject: engineering
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a;
+printf("Enter the number or value\n ");
+scanf("%d",&a);
+if (a%5==0&&a%11==0)
+{
+printf("The value is divisible by 5 and 11 that is %d",a);
+}
+else
+{printf("the value is not divisible",a);
+}
+getch ();
+return 0;
+
+
+}
