2fd04c83d3750e13d10110877c2d1fab8940a27a
change G.L Joshi <change.joshi247@gmail.com>
2016-12-21 10:28:01 +0545

    Add files via upload

---
+++ vowels using ternary.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<Conio.h>
+int main (){
+char c;
+printf("Enter the alphabet \n");
+scanf("%c",&c);
+ (c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+?printf("the alphabet is vowel"):printf("The alphabet is not vowel");
+getch();
+return 0;
+
+}
+
