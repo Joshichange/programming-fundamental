e13dad495be3407657dd5fb7a1bd8b885c872c13
change G.L Joshi <change.joshi247@gmail.com>
2016-12-21 10:57:32 +0545

    Add files via upload

---
+++ checkingalphabet,fromasciivalue,ternary.c
@@ -0,0 +1,19 @@
+/*change joshi
+12
+programming fundamental
+labsheet:14
+
+*/
+#include<stdio.h>
+#include<conio.h>
+int main (){
+int asciivalue;
+char a;
+printf("Enter the alphabhet\n");
+scanf("%c",&a);
+asciivalue==a;
+(a>=65&&a<=97||a>=97&&a<=122)
+?printf("The character is alphabet"):printf("the character is not alphabet");
+getch();
+return 0;
+}
